d8f949610dcf94cff785177c18da6211458f7235
Murray Rogers <murray.rogers@metaswitch.com>
2019-12-02 16:25:09 +0000

    Remove more code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     let (mut total, content) = (0, std::fs::read_to_string("inputs.txt").unwrap());
-    let _ : Vec<&str> = content.lines().collect::<Vec<&str>>().iter().map(|input| {total += input.clone().parse::<u64>().unwrap()/3 - 2; println!("{}", total); *input}).collect();   
+    content.lines().collect::<Vec<&str>>().iter().for_each(|input| {total += input.clone().parse::<u64>().unwrap()/3 - 2; println!("{}", total);});   
 }
